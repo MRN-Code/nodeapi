f2d9c130ecbc533fc09f662020ba7cf9af75c797
Dylan Wood <dwood@mrn.org>
2015-07-24 14:26:53 -0700

    add harmony_arrow_functions to mocha tests

--- grunt/mochacli.js
+++ grunt/mochacli.js
@@ -2,7 +2,9 @@
 module.exports = function(grunt) {
     return {
         options: {
-            files: 'test/*_test.js'
+            files: 'test/*_test.js',
+            harmony: true,
+            harmony_arrow_functions: true
         },
         spec: {
             options: {

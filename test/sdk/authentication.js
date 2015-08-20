c9b11a5e077fa953bbc2cd12ac38f35b9591376f
Cory Reed <creed@mrn.org>
2015-08-20 08:57:15 -0700

    Refactor `btoa` to be injected.

--- test/sdk/authentication.js
+++ test/sdk/authentication.js
@@ -4,45 +4,32 @@
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define([], factory);
+        define([], factory(btoa));
     } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory();
+        var base64 = (function() {
+            if (typeof btoa !== 'undefined') {
+                return btoa;
+            }
+
+            return require('btoa');
+        })();
+
+        module.exports = factory(base64);
     } else {
         // Browser globals (root is window)
         root.CoinsApiClient = root.CoinsApiClient || {};
-        root.CoinsApiClient.authentication = factory();
+        root.CoinsApiClient.authentication = factory(root.btoa);
     }
-}(this, function() {
+}(this, function(btoa) {
 
     'use strict';
 
     var baseUri = '/auth';
 
     var me;
-
-    /**
-     * Set me.btoa for base64 encoding in a browser-compatible way.
-     * IE < 10 not supported
-     * @return {object} me
-     */
-    function setBtoa() {
-        if (typeof btoa === 'function') {
-            //we are in browser, and btoa will be defined for us
-            me.btoa = btoa;
-        } else if (typeof Buffer === 'function') {
-            //probably in Node.js environment
-            me.btoa = function(str) {
-                return new Buffer(str).toString('base64');
-            };
-        } else {
-            throw new Error('Envonment unsupported: need `btoa` or `Buffer`');
-        }
-
-        return me;
-    }
 
     /**
      * generate the authentication payload expected by POST /auth/keys route
@@ -52,8 +39,8 @@
      */
     function generateLoginPayload(username, password) {
         return {
-            username: me.btoa(username),
-            password: me.btoa(password)
+            username: btoa(username),
+            password: btoa(password)
         };
     }
 
@@ -151,7 +138,6 @@
      */
     function init(base) {
         me = base;
-        setBtoa();
         return {
             login: login,
             logout: logout

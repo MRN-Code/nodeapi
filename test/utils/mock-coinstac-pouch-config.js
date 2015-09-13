d8f4cd31141e7700ee4626b64d6d86bf8352897d
Dylan Wood <dwood@mrn.org>
2015-09-13 18:10:09 +0000

    load coinstac routes during tests

--- test/utils/mock-coinstac-pouch-config.js
+++ test/utils/mock-coinstac-pouch-config.js
@@ -5,6 +5,8 @@
  */
 
 process.env.ALLOW_CONFIG_MUTATIONS = true;
+const cliOpts = require('../../lib/utils/cli-options.js');
+cliOpts.coinstac = true;
 const config = require('config');
 const _ = require('lodash');
 const origConfig = config.get('coinstac.pouchdb');

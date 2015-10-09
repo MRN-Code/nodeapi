2319331477d82416167300fdff7e39eae5906df6
cdaringe <cdieringer@mrn.org>
2015-10-08 17:43:23 -0700

    load babel polyfill on test start

--- grunt/mochacli.js
+++ grunt/mochacli.js
@@ -3,7 +3,7 @@ module.exports = function() {
     return {
         options: {
             files: ['test/integration/*.js', 'test/unit/*.js'],
-            require: ['test/utils/mock-coinstac-pouch-config.js'],
+            require: ['test/init.js'],
             harmony: true,
             /* jscs:disable */
             harmony_arrow_functions: true //jshint ignore:line

a7fb8942ea7c5f824cfa573c76da95b5bdd99ad7
cdaringe <cdieringer@mrn.org>
2015-10-08 17:53:32 -0700

    move build to postinstall

--- dist/lib/models/Scan.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function (bookshelf) {
-    return bookshelf.extend({
-        tableName: 'mrs_scan_sessions'
-    });
-};
--- dist/lib/models/UserStudyRolePriv.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function (bookshelf) {
-    return bookshelf.extend({
-        tableName: 'casdba.cas_study_user_role_privs'
-    });
-};

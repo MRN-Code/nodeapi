4661312f37acfdaa6b4206d3adae42fc214b3542
cdaringe <cdieringer@mrn.org>
2015-10-09 12:12:31 -0700

    add grunt build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,5 @@ module.exports = function(grunt) {
     grunt.registerTask('lint', ['jshint', 'jscs']);
     grunt.registerTask('test', ['mochacli']);
     grunt.registerTask('default', ['lint', 'test']);
-    grunt.registerTask('build', function() {
-        grunt.log.ok('No build task configured');
-    });
+    grunt.registerTask('build', ['exec:npm-run-build']);
 };

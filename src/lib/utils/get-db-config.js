b58b8585ecd8233cd02086875626bf54e4dff47b
Dylan Wood <dwood@mrn.org>
2015-11-01 06:01:42 +0000

    parse new /coins/config/dbmap.json file to get DB config

--- src/lib/utils/get-db-config.js
+++ src/lib/utils/get-db-config.js
@@ -1,39 +1,30 @@
 'use strict';
 
 const config = require('config');
-const dbMap = require(config.get('dbMapPath'));
 const _ = require('lodash');
+const dbMap = require(config.get('dbMapPath'));
 
-const envMap = {
-    production: 'prd',
-    staging: 'training',
-    development: 'dev'
-};
+const appName = 'nodeApi';
+const env = process.env.COINS_ENV;
+const globalDefaults = dbMap._default;
+const appDefaults = dbMap._apps[appName];
 
 /**
  * get DB connection parameters, based on COINS_ENV environment var
  * @return {object} DB connection parameters
  */
 function getDbConfig() {
-    let dbConfig;
-    let dbDefaults;
-    const env = envMap[process.env.COINS_ENV];
-    if (dbConfig === undefined) {
-        dbDefaults = dbMap.default;
-        if (envMap[process.env.COINS_ENV] !== undefined) {
-            dbConfig = dbMap[env].nodeApi;
-            return _.defaults(dbConfig, dbDefaults);
-        } else {
-            throw new Error(
-                `unrecognised database environment: '${process.env.COINS_ENV}'`
-            );
-        }
-    } else {
+    let envAppDefaults;
+    let envDefaults;
+    if (dbMap[process.env.COINS_ENV] === undefined) {
         throw new Error(
-            `could not load database config at '${config.get('dbMapPath')}'`
+            `unrecognised database environment: '${process.env.COINS_ENV}'`
         );
     }
 
+    envDefaults = dbMap[env]._default || {};
+    envAppDefaults = dbMap[env][appName];
+    return _.merge(globalDefaults, appDefaults, envDefaults, envAppDefaults);
 }
 
 module.exports = getDbConfig;

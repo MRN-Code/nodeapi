a7db738a0a58146f74a6c8d94c728db3b36f1554
Niranjan Humagain <nhumagain@mrn.org>
2016-01-04 17:27:08 +0000

    added series and series data controllers, added validation to scan details, passed path parameters to half penny dynamically

---
+++ src/lib/controllers/series.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const joi = require('joi');
+
+/**
+ * joi.any().required(), // TODO waiting on ‘joi.extend’
+ * to handle nullable property`
+ */
+module.exports.seriesSchema = joi.object().keys({
+    seriesId: joi.number().required(),
+    modalityId: joi.number().required(),
+    scanId: joi.number().required(),
+    basePath: joi.string().required(),
+    studyCodeId: joi.number().required(),
+    usable: joi.string().required(),
+    notes: joi.string().required(),
+    sortOrder: joi.number().required(),
+    labelId: joi.number().required(),
+    dxExcluded: joi.boolean().required(),
+});

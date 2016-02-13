0389592caf198d36dc2386da2cab7a6885784f80
Ross Kelly <rkelly@mrn.org>
2016-02-12 17:59:45 -0700

    fix url matching

--- src/lib/utils/strip-api-url-prefix.js
+++ src/lib/utils/strip-api-url-prefix.js
@@ -5,13 +5,13 @@ const url = require('url');
 module.exports.register = function(server, options, next) {
     server.ext('onRequest', (request, reply) => {
         const newPath = request.url.pathname || request.url.path || request.url.href;
-        if (newPath.indexOf('/api/v.') < 0) {
+        if (newPath.match(/\/api\/v[0-9]+\.[0-9]+\.[0-9]+/) === null) {
             return reply.continue();
         }
         let newUrl = _.clone(request.url);
         delete newUrl.path;
         delete newUrl.href;
-        newUrl.pathname = newPath.replace(/\/api\/v\.[^\/]*/, '');
+        newUrl.pathname = newPath.replace(/\/api\/v[0-9]+\.[0-9]+\.[0-9]+/, '');
         request.setUrl(url.format(newUrl));
         reply.continue();
     });

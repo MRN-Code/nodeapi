3ff9948b0ae305ed8253d76ded8c7e09cca9bf83
cdaringe <christopher.dieringer@gmail.com>
2016-03-29 18:09:11 -0600

    babel es6 code for the API client-only until uglifyJS supports es6

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,14 @@ module.exports = {
             {
                 test: /\.json$/,
                 loader: 'json'
+            },
+            {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel', // 'babel-loader' is also a legal name to reference
+              query: {
+                presets: ['es2015']
+              }
             }
         ]
     },
